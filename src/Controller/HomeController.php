bfd44a9ca9473f0d981d0b4f4c6314bdcbb5f598
Oriol Buqueras Solsona <oriolbuqueras@gmail.com>
2020-09-02 18:54:50 +0100

    ➕ Add Bootstrap and JQuery to the project

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+class HomeController extends AbstractController
+{
+    public function index(Request $request)
+    {
+        return $this->render('index.html.twig');
+    }
+}
